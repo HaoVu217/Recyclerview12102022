8986c1703df9c3f084857f0604fe6f9250b44f2d
HaoVu217 <kiet21795@gmail.com>
2022-11-29 15:59:12 +0700

    Store

--- app/src/main/java/com/example/recyclerview12102022/ServiceType.java
+++ app/src/main/java/com/example/recyclerview12102022/ServiceType.java
@@ -1,5 +1,22 @@
 package com.example.recyclerview12102022;
 
 public enum ServiceType {
+    RESTAURANT("Nhà hàng"),
+    BIRTH_DAY("Sinh Nhật"),
+    FAMILY("Gia Đình"),
+    GROUP("Hội Nhóm"),
+    SHOP_ONLINE("Shop Online"),
+    STREET_FOOD("Đồ Ăn Đường Phố"),
+    BUFFET("Buffer");
+
+    private final String name;
+
+    ServiceType(String s){
+        name = s;
+    }
+
+    public String toString(){
+        return name;
+    }
 
 }
